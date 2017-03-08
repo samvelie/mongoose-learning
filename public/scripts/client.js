82279f6991a96022a4f871ddb7b18f21d6c07158
Luke Schlangen <lukeschlangen@gmail.com>
2017-03-08 14:23:49 -0600

    moving about controller

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -21,10 +21,3 @@ myApp.config(['$routeProvider', function($routeProvider) {
       redirectTo: 'home'
     })
 }]);
-
-
-myApp.controller('AboutController', function(){
-  console.log('About Controller was loaded');
-  var self = this;
-  self.listOfPeople = ['Phil', 'Tom', 'Jeremy', 'Celina', 'Kris'];
-});
---
+++ public/scripts/controllers/about.controller.js
@@ -0,0 +1,5 @@
+myApp.controller('AboutController', function(){
+  console.log('About Controller was loaded');
+  var self = this;
+  self.listOfPeople = ['Phil', 'Tom', 'Jeremy', 'Celina', 'Kris'];
+});
