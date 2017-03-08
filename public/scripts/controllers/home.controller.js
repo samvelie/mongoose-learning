f2322e9299ae53ecfd82c6b36ba4df0a7e735e0a
Luke Schlangen <lukeschlangen@gmail.com>
2017-03-08 16:13:37 -0600

    removing extra properties in API in home controller

--- public/scripts/controllers/home.controller.js
+++ public/scripts/controllers/home.controller.js
@@ -2,6 +2,4 @@ myApp.controller('HomeController', ['TaskFactory', function(TaskFactory){
   console.log('Home Controller was loaded');
   var self = this;
   self.someMessage = 'This site is amazing!!';
-  self.messageFromTheFactory = TaskFactory.testProperty;
-  self.arrayFromTheFactory = TaskFactory.testArray;
 }]);
